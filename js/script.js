28ca042a126f91a6783a9fc78cc8ab86e8edbb9d
Dima <example@gmail.com>
2025-11-17 22:31:02 +0100

    added rest operator to class

--- js/script.js
+++ js/script.js
@@ -154,12 +154,13 @@ function showModalByScroll () {
 window.addEventListener('scroll', showModalByScroll);
 
 class MenuCard {
-	constructor (src, alt, title, descr, price, parentElement) {
+	constructor (src, alt, title, descr, price, parentElement, ...classes) {
 		this.src = src;
 		this.alt = alt;
 		this.title = title;
 		this.descr = descr;
 		this.price = price;
+		this.classes = classes;
 		this.parentElement = document.querySelector(parentElement);
 		this.transfer = 27;
 		this.changeToUAH();
@@ -171,18 +172,22 @@ class MenuCard {
 
 	render() {
 		const div = document.createElement('div');
+		if(this.classes.length === 0) {
+			this.classes.push('menu__item');
+		}
+		this.classes.forEach((item) => {
+			div.classList.add(item);
+		});
 		div.innerHTML = `
-			<div class="menu__item">
-                    <img src=${this.src} alt=${this.alt}>
-                    <h3 class="menu__item-subtitle">${this.title}</h3>
-                    <div class="menu__item-descr">${this.descr}</div>
-                    <div class="menu__item-divider"></div>
-                    <div class="menu__item-price">
-                        <div class="menu__item-cost">Цена:</div>
-                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-                    </div>
-                </div>
-		`;
+			<img src=${this.src} alt=${this.alt}>
+			<h3 class="menu__item-subtitle">${this.title}</h3>
+			<div class="menu__item-descr">${this.descr}</div>
+			<div class="menu__item-divider"></div>
+			<div class="menu__item-price">
+				<div class="menu__item-cost">Цена:</div>
+				<div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+			</div>
+`;
 		this.parentElement.append(div);
 	}
 }
